d305846edd34daf2b9dc311d2774af0c848fb59b
Rajgithubtrial <rajashekar.kalyanam@capgemini.com>
2022-12-23 15:15:39 +0530

    testing multi script changes with different Script Types

--- Scripts/R__ReplaceTest.sql
+++ Scripts/R__ReplaceTest.sql
@@ -1,6 +1,24 @@
 use schema CICD_POC;
 
-Create Or Replace Table 
+create table if not exists   
 Test_Replace_table
 (Test1 VARCHAR(10)
  , TEST2 VARCHAR(20));
+ 
+ create table if not exists  
+Test_Replace_table1
+(Test1 VARCHAR(10)
+ , TEST2 VARCHAR(20));
+ 
+ 
+ create table if not exists  
+Test_Replace_table2
+(Test1 VARCHAR(10)
+ , TEST2 VARCHAR(20));
+ 
+ /*
+ create table if not exists  
+Test_Replace_table1
+(Test1 VARCHAR(10)
+ , TEST2 VARCHAR(20));
+ */
---
+++ Scripts/R__ReplaceTest.sql.bak
@@ -0,0 +1,6 @@
+use schema CICD_POC;
+
+Create Or Replace Table 
+Test_Replace_table
+(Test1 VARCHAR(10)
+ , TEST2 VARCHAR(20));
