a31a015f1c5bf62c7f83f5ae92987d9aff332c6e
Rajgithubtrial <rajashekar.kalyanam@capgemini.com>
2022-12-23 15:59:32 +0530

    test A & R files

--- Scripts/A__AlwaysScriptTest.sql
+++ Scripts/A__AlwaysScriptTest.sql
@@ -9,10 +9,3 @@ create table if not exists
 Test_Always_script_schemaChng1
 (Test1 VARCHAR(10)
  , TEST2 VARCHAR(20));
- /*
- create table if not exists  
-Test_Always_script_schemaChng2
-(Test1 VARCHAR(10)
- , TEST2 VARCHAR(20));
- 
- */
--- Scripts/R__ReplaceTest.sql
+++ Scripts/R__ReplaceTest.sql
@@ -15,10 +15,3 @@ Test_Replace_table1
 Test_Replace_table2
 (Test1 VARCHAR(10)
  , TEST2 VARCHAR(20));
- 
- /*
- create table if not exists  
-Test_Replace_table1
-(Test1 VARCHAR(10)
- , TEST2 VARCHAR(20));
- */
